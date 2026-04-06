3ea817bf3f55e8dd8b56f0d6091c490694dfec25
Balu Babu <balub997@gmail.com>
2026-04-06 23:32:54 +0530

    chore: restructured monorepo

--- apps/frontend-kiosk/vite.config.ts
+++ apps/frontend-kiosk/vite.config.ts
@@ -1,15 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
   server: {
     host: "::",
-    port: 8080,
+    port: 8081,
   },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  plugins: [react()],
   resolve: {
     alias: [
       {
@@ -26,4 +25,5 @@ export default defineConfig(({ mode }) => ({
       },
     ],
   },
-}));
+});
+
